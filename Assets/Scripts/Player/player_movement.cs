4be72456b64c3c7d7fb5bec091a2632b2441d540
Grodem <Lukas.hundertmark@googlemail.com>
2015-12-17 23:25:57 +0100

    nothing of interest

--- Assets/Scripts/Player/player_movement.cs
+++ Assets/Scripts/Player/player_movement.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class player_movement : MonoBehaviour {
 
+    //Variables
+    private double gravity;
+    
+
 	// Use this for initialization
 	void Start () {
+        gravity = 0.2;
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+    }
 }
