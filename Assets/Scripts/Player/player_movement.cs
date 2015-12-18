58ec1076c45e188892da9a625a6edd4c428a9c33
Grodem <Lukas.hundertmark@googlemail.com>
2015-12-18 18:51:32 +0100

    player movement
    Have to work out how to use the physics engine correctly.

--- Assets/Scripts/Player/player_movement.cs
+++ Assets/Scripts/Player/player_movement.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class player_movement : MonoBehaviour {
+public class player_movement : MonoBehaviour
+{
 
     //Variables
-    private double gravity;
-    
+    public float movementSpeed;
 
-	// Use this for initialization
-	void Start () {
-        gravity = 0.2;
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        Ray ray = new Ray(transform.position, Vector3.down);
 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
     }
 }
