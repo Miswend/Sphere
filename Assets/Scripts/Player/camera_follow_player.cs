724a790aafd7fd639957b142b2b575b4938788d0
Grodem <Lukas.hundertmark@googlemail.com>
2015-12-20 16:01:07 +0100

    player follwing camera

---
+++ Assets/Scripts/Player/camera_follow_player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_follow_player : MonoBehaviour {
+
+    public Transform playerTransform;
+
+    private Vector3 offset;
+    private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+        cameraTransform = GetComponent<Transform>();
+        offset = cameraTransform.position - playerTransform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cameraTransform.position = playerTransform.position + offset;
+	}
+}
